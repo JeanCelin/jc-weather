cd7abec201686facf7c24ff2f2bb4c7f0db88caf
Jean Celin <jean_estudos@hotmail.com>
2025-01-04 11:17:08 -0300

    adjustments in the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <header>
+    <>
       <p>JC WEATHER</p>
       <Search />
-    </header>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <header className={styles.home__header}>
+        <Header />
+      </header>
+      <main
+      className={styles.home__content}
+      >Conteúdo</main>
     </>
   );
 }
