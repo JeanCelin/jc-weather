377e83029652e1aa92da8aa2427ed1b3de3b5452
Jean Celin <jean_estudos@hotmail.com>
2025-01-04 10:24:55 -0300

    add font and theme colors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
