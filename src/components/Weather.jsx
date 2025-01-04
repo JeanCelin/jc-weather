6e6229cf2e3e84dd243e282d6deac75812403f78
Jean Celin <jean_estudos@hotmail.com>
2025-01-04 13:50:40 -0300

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,13 +9,20 @@ export default function Weather({ data, error, loading }) {
 
   const iconCode = data.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-  console.log(iconCode);
+
+  const timestamp = data.dt;
+  const date = new Date(timestamp * 1000);
+  console.log(date.toLocaleString("en-US"));
+  console.log(data.weather[0].id)
   return (
     <div>
       <h1>{data.name}</h1>
-      <Image src={iconUrl} width={64} height={64} />
+      <section>
+        <p>{date.toString()}</p>
+        <Image src={iconUrl} width={64} height={64} alt={data.weather[0].main}/>
+        <p> {data.weather[0].description}</p>
+      </section>
       <p>Temperatura: {data.main.temp}°C</p>
-      <p>Descrição: {data.weather[0].description}</p>
     </div>
   );
 }
