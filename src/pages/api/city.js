905b3175e17fb500dee9c9d34f191eed662ee0f4
Jean Celin <jean_estudos@hotmail.com>
2026-03-02 08:03:39 -0300

    feat: add  handle errors in API route

--- src/pages/api/city.js
+++ src/pages/api/city.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const { query } = req.query;
-
-  const key = process.env.OPENWEATHER_KEY;
-
   try {
+    const { query } = req.query;
+    const key = process.env.OPENWEATHER_KEY;
+
+    console.log("KEY EXISTS?", !!process.env.OPENWEATHER_KEY);
+    if (!query || !key) throw new Error(`Erro nos parâmetros da API City`);
+
     const response = await axios.get(
       `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${key}`,
     );
+
+
+
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ message: "Erro ao buscar clima" });
--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -1,22 +1,18 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-
-  const { lat, lon, lang } = req.query;
-
-
-  const key = process.env.OPENWEATHER_KEY;
- 
   try {
+    const { lat, lon, lang } = req.query;
+    const key = process.env.OPENWEATHER_KEY;
+    console.log("KEY EXISTS?", !!process.env.OPENWEATHER_KEY);
+    if (!lat || !lon || !lang || !key)
+      throw new Error(`Erro nos parametros da API`);
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric&lang=${lang}`,
     );
 
-    
-
     res.status(200).json(response.data);
   } catch (error) {
- 
     res.status(500).json({ message: "Erro ao buscar clima" });
   }
 }
